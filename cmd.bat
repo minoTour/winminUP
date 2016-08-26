92d91e6388def3c74fdad3f85d884299d6679d1a
Matt Loose <matt.loose@nottingham.ac.uk>
2016-08-26 17:54:30 +0100

    Minor fixes for R9

--- cmd.bat
+++ cmd.bat
@@ -1,1 +1,1 @@
-c:\Python27\python.exe minUP.py -bwa  -cs 2016-05-13_17:06:52 -d -dbh localhost -dbp 3306 -dbu Mike1 -f "C:\DeepSEQ_WIP\referenceGenomes\Zika_Africa.fasta" -n 4 -name R9_test -o minionowner -procs 4 -pw 1234 -r 0 -u Mike1 -useHdfTimes -v -w "C:\DeepSEQ_WIP\reads\Zika_R9"
+c:\Python27\python.exe minUP.py -d -dbh localhost -dbp 3306 -dbu Mike1 -n 1 -name 20160825 -o minionowner -procs 1 -pw 1234 -r 0 -u Mike1 -v high -pre -standalone -w  "C:\DeepSEQ_WIP\reads\sunir_tests\reads"
